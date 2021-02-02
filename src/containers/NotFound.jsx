bb33a52caaceb87e245b541272b02c0005390a4d
jessica <jessi.sandoval93@gmail.com>
2021-02-02 17:11:56 -0500

    add actions and reducers to make delete and addFavorites items

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/Register.scss';
+
+const NotFound = () => (
+  <>
+    <h2>Page Not Found</h2>
+    <h3>Regresa al home</h3>
+  </>
+);
+
+export default NotFound;
