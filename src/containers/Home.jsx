3470ea26ecf0c90b0b7a3668c5b72f6d5bff2387
jessica <jessi.sandoval93@gmail.com>
2021-02-03 16:09:51 -0500

    validations UI

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,11 +5,13 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import '../assets/styles/App.scss';
+import Header from '../components/Header';
 
 const Home = ({ mylist, trends, originals }) => {
   return (
     <>
-      <Search />
+      <Header />
+      <Search isHome />
       {mylist.length > 0 &&
         <Categories title='Mi lista'>
           <Carousel>
